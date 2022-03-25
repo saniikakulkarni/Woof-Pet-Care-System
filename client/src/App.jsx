887c5ac1c22756c8991cb547c0313c9a72c317a1
Sanika Kulkarni <64094450+saniikakulkarni@users.noreply.github.com>
2022-03-25 09:31:57 +0530

    Router added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,16 @@
-import { useState, useEffect } from "react";
-// import Login from "./Pages/Login"
+// import { useState, useEffect } from "react";
+import React from 'react';   
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom' 
+import Home from "./Pages/Home"
+import Login from "./Pages/Login"
 import Signup from "./Pages/Signup";
 
+
 // functions
-import { getTest } from "./functions/test";
+// import { getTest } from "./functions/test";
 
 function App() {
-	const [data, setData] = useState("Hello World!");
+	// const [data, setData] = useState("Hello World!");
 
 	// useEffect(() => {
 	// 	getTest()
@@ -15,9 +19,14 @@ function App() {
 	// }, []);
 
 	return (
-		<div >
-			<Signup></Signup>
-			{/* <Login/> */}
+		<div className="App">
+			<Router>
+				<Routes>
+					<Route path="/" element={<Home/>}/>
+					<Route path="/login" element={<Login />}/>
+					<Route path="/signup" element={<Signup />}/>
+        		</Routes> 
+			</Router>
 		</div>
 	);
 }
