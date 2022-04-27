e7144c44b988505c2379b594220c6e7ef002a48e
Sanika Kulkarni <64094450+saniikakulkarni@users.noreply.github.com>
2022-04-27 10:54:42 +0530

    Profile page updated

--- client/src/Pages/PetOwner/OwnerProfile.js
+++ client/src/Pages/PetOwner/OwnerProfile.js
@@ -7,17 +7,13 @@ const OwnerProfile = () => {
   return (
     <>
     <Navbar/>
-    <div class="container">
-        <div class="col-md-10 col-xl-10">
+    <div class="container mt-5">
+        <div class="col-md-10 col-xl-9 m-auto">
             <div class="tab-content">
                 <div class="tab-pane fade show active" id="account" role="tabpanel">
-
                     <div class="card top-border-radius">
                         <div class="card-header">
-                            <div class="card-actions float-right">
-                                <h5 class="card-title mb-0">Pet Owner</h5>
-                            </div>
-                            <h5 class="card-title mb-0">Public info</h5>
+                            <h5 class="card-title mb-0">Pet Owner Details</h5>
                         </div>
                         <div class="card-body">
                             <form>
@@ -41,19 +37,15 @@ const OwnerProfile = () => {
                                             <p>Age</p>
                                          </div>
                                          <div class="form-group">
-                                            <p>Country</p>
+                                            <p>Address</p>
                                          </div>
                                          <div class="form-group">
-                                            <p>State</p>
-                                         </div>
-                                         <div class="form-group">
-                                            <p>City</p>
+                                            <p>Govt ID</p>
                                          </div>
                                     </div>
                                     <div class="col-md-4">
                                         <div class="text-center">
                                             <img alt="" src="" class="rounded-circle img-responsive mt-2" width="128" height="128"/>
-                                            
                                         </div>
                                     </div>
                                 </div>
@@ -64,52 +56,27 @@ const OwnerProfile = () => {
                     
                     <div class="card">
                         <div class="card-header">
-                            <div class="card-actions float-right">
-                                <h5 class="card-title mb-0">Pet Owner</h5>
-                            </div>
-                            <h5 class="card-title mb-0">Private info</h5>
+                            <h5 class="card-title mb-0">Pet Deatils</h5>
                         </div>
                         <div class="card-body">
                             <form>
                                 <div class="form-group">
-                                    <p>Address</p>
+                                    <p>Name</p>
                                 </div>
                                 <div class="form-group">
-                                    <p>Govt ID</p>
+                                    <p>Breed</p>
                                 </div>
-                                
+                                <div class="form-group">
+                                    <p>Likes</p>
+                                </div>
+                                <div class="form-group">
+                                    <p>Dislikes</p>
+                                </div>  
                             </form>
-
                         </div>
                     </div>
-                  <Link to='/owner/profileedit' className="btn btn-lg btn-primary px-3 d-none d-lg-block mx-2">Edit</Link>
+                  <Link to='/owner/profileedit' className="btn btn-lg btn-primary px-3 d-none d-lg-block mx-2 w-25 mb-5 ">Edit</Link>
                 </div>
-                <div class="tab-pane fade" id="password" role="tabpanel">
-                    <div class="card">
-                        <div class="card-body">
-                            <h5 class="card-title">Password</h5>
-                            <form>
-                                <div class="form-group">
-                                    <label for="inputPasswordCurrent">Current password</label>
-                                    <input type="password" class="form-control" id="inputPasswordCurrent"/>
-                                    <small><a href="#">Forgot your password?</a></small>
-                                </div>
-                                <div class="form-group">
-                                    <label for="inputPasswordNew">New password</label>
-                                    <input type="password" class="form-control" id="inputPasswordNew"/>
-                                </div>
-                                <div class="form-group">
-                                    <label for="inputPasswordNew2">Verify password</label>
-                                    <input type="password" class="form-control" id="inputPasswordNew2"/>
-                                </div>
-                                <button type="submit" class="btn btn-primary">Save changes</button>
-                            </form>
-
-                        </div>
-                    </div>
-                </div>
-
-                
             </div>
         </div>
 </div>
